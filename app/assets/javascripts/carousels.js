ba3c8f1194cb67d541ddbe3ad092dcc41cd43930
helwina <morphee3301@hotmail.fr>
2016-12-01 18:38:46 +0100

    changement de l intervar du carrousel

--- app/assets/javascripts/carousels.js
+++ app/assets/javascripts/carousels.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('.carousel').carousel({ interval: 2000 });
+    $('.carousel').carousel({ interval: 2750 });
     $('#previous').click(function() { $('.carousel').carousel('prev'); });
     $('#pause').click(function() { $('.carousel').carousel('pause'); });
     $('#play').click(function() { $('.carousel').carousel('cycle'); });
